40a872763f16696a7bc25fa9babdde2b06c7a28f
nushiamme <nushiamme@gmail.com>
2013-11-27 09:41:14 -0500

    Trying anova, added habitat stream boxplot, modified lm and anova

--- Anco_sept13.R
+++ Anco_sept13.R
@@ -2,6 +2,8 @@
 require(ggplot2)
 require(reshape)
 require(plyr)
+require(boot)
+require(stats)
 
 ## Set working directory
 setwd("C://Users//Anusha/Desktop/R scripts/hornbills")
@@ -31,11 +33,12 @@ for(i in 1:n) {
 # Check if it worked
 anco$dbhcl
 
-# Trying a melt function
-m.anco <- melt(data=anco, id.vars=c("Number", "habitat2", "nestht"),
-               measure.vars= "Building", na.rm =T)
+## Seeing how to do an Anova with habitat (categorial, factors) as the measure variable
+anco$hab_binary[anco2$habitat2=="Open"] <- 0
+anco$hab_binary[anco2$habitat2=="Forest"] <- 1
 
-### Plots ###
+###-------- Plots ---------###
+
 ## Nest tree sps vs. dbh
 sps_dbh <- ggplot(anco, aes(x=Species,y=dbh)) + xlab("Tree Species") + 
   geom_point(col="darkgreen") + theme_bw()
@@ -46,17 +49,22 @@ habitat_building_box <- ggplot(anco, aes(x=habitat2,y=Building)) + xlab("Habitat
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 habitat_building_box
 
+## Habitat vs. distance from stream
+habitat_stream <- ggplot(anco, aes(x=habitat2,y=Stream)) + xlab("Habitat Type") + 
+  theme_bw() + geom_boxplot(col="darkgreen", size=0.3)
+habitat_stream
+
 ## Comparing building and stream distances vs habitat. 
 # Need to add legend- red is building, blue is stream
 habitat_building <- ggplot(anco, aes(x=habitat2,y=Building)) + xlab("Habitat") +
   ylab("Distance") + geom_point(col="red", size=3) + theme_bw()
 habitat_building + geom_point(data=anco, aes(x = habitat2, y = Stream), col="blue", size=3)
 
-
 ## dbh vs. habitat
 habitat_dbh <- ggplot(anco, aes(x=habitat2,y=dbh)) + xlab("Habitat Type") + 
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 habitat_dbh
+## As long as you have big trees in open habitat, good. 
 
 ## tree ht vs. habitat
 habitat_treeht <- ggplot(anco, aes(x=habitat2,y=ht)) + xlab("Habitat Type") + 
@@ -70,7 +78,7 @@ habitat_nestht
 
 hab_nesthtbox <- ggplot(anco, aes(x=habitat2,y=nestht)) + xlab("Habitat Type") +
   ylab("Nest Height") +  geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
-hab_nesthtbox
+hab_nesthtbox ## Check outlier
 
 ### dbh vs. distance to Building
 dbh_building <- ggplot(anco, aes(x=Building,y=dbh)) + xlab("Distance to habitation") +
@@ -102,6 +110,8 @@ box_anco<-ggplot(m.anco, aes(x=habitat,y=nestht)) +
 box_anco
 
 ## Statistical Analyses
+## Nest trees have similar characteristics as in the forest, and they seem to "find" them.
+## 
 
 # Test for normality- shapiro.test()
 shapiro.test(anco$dbh)
@@ -109,13 +119,21 @@ shapiro.test(anco$ht)
 shapiro.test(anco$nestht)
 shapiro.test(anco$Stream) # Not normal
 
+#### TO DO- refine distances so that they're not right censored! #######
+## Test for correlations between variables.
+cor.test(anco$Stream, anco$Road, method="pearson")
+
 # Two sample t test for distances
 t.test(x=anco$Building[anco$habitat2=="Open"], y=anco$Building[anco$habitat2=="Forest"])
 t.test(anco$dbh[anco$Species=="Mangifera indica"], anco$dbh[anco$Species=="Terminalia bellerica"])
 t.test(x=anco$Stream[anco$habitat2=="Open"], y=anco$Stream[anco$habitat2=="Forest"])
+t.test(x=anco$Road, y=anco$Building)
 ## Useful? What does this mean?
 ks.test(x=anco$Stream, y=anco$Building)
 
 # Trying linear models to look at what affects nest choice
-anco.lm <- lm(dbh ~ Stream + Building + habitat2 + nestht, anco)
+anco.lm <- lm(hab_binary ~ Stream + Building + nestht + dbh, anco)
 anova(anco.lm)
+
+anco.lm2 <- lm(dbh ~ Species + nestht + ht, anco)
+anova(anco.lm2)
