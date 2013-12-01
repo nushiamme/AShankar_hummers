6749fe0a257540d0c9f7d893de0137c8c273e6f5
nushiamme <nushiamme@gmail.com>
2013-11-30 21:01:14 -0500

    Replaced dataset with more accurate habitat param data from Google earth
    Used GE to get nest distances to stream, building, road. Mainly to
    prevent right censoring, but also found that one point was a GH point
    (changed Lowale to Kosumb)

--- Anco_sept13.R
+++ Anco_sept13.R
@@ -9,9 +9,13 @@ require(stats)
 setwd("C://Users//Anusha/Desktop/R scripts/hornbills")
 
 ## Read in data
-anco <- read.csv("Ancodata_Sept2013.csv")
+#anco2 <- read.csv("Ancodata_Sept2013.csv")
+anco <- read.csv("Ancodata_edit.csv")
 
 ## Cleaning and aggregating data
+
+## Subset necessary columns
+anco <- anco[,c(1,2,5,6,7,8,10,11,12,16,20,21,22)]
 
 ### Sort dbh's into size classes. Note: Sort dataset by increasing dbh before doing this.
 anco$dbhcl <- 0
@@ -41,42 +45,43 @@ anco$hab_binary[anco$habitat2=="Forest"] <- 1
 
 ## Nest tree sps vs. dbh
 sps_dbh <- ggplot(anco, aes(x=Species,y=dbh)) + xlab("Tree Species") + 
-  geom_point(col="darkgreen") + theme_bw()
+  geom_point(col="darkgreen") + theme_bw()+ 
+  theme(axis.text.x=element_text(angle=60, vjust=1, hjust=1))
 sps_dbh
 
 ## Habitat vs. distance from building
-habitat_building_box <- ggplot(anco, aes(x=habitat2,y=Building)) + xlab("Habitat Type") + 
+habitat_building_box <- ggplot(anco, aes(x=habitat,y=Building)) + xlab("Habitat Type") + 
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 habitat_building_box
 
 ## Habitat vs. distance from stream
-habitat_stream <- ggplot(anco, aes(x=habitat2,y=Stream)) + xlab("Habitat Type") + 
-  theme_bw() + geom_boxplot(col="darkgreen", size=0.3)
+habitat_stream <- ggplot(anco, aes(x=habitat,y=Stream)) + xlab("Habitat Type") + 
+  theme_bw() + geom_point() # geom_boxplot(col="darkgreen", size=0.3)
 habitat_stream
 
 ## Comparing building and stream distances vs habitat. 
 # Need to add legend- red is building, blue is stream
-habitat_building <- ggplot(anco, aes(x=habitat2,y=Building)) + xlab("Habitat") +
+habitat_building <- ggplot(anco, aes(x=habitat,y=Building)) + xlab("Habitat") +
   ylab("Distance") + geom_point(col="red", size=3) + theme_bw()
-habitat_building + geom_point(data=anco, aes(x = habitat2, y = Stream), col="blue", size=3)
+habitat_building + geom_point(data=anco, aes(x = habitat, y = Stream), col="blue", size=3)
 
 ## dbh vs. habitat
-habitat_dbh <- ggplot(anco, aes(x=habitat2,y=dbh)) + xlab("Habitat Type") + 
+habitat_dbh <- ggplot(anco, aes(x=habitat,y=dbh)) + xlab("Habitat Type") + 
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 habitat_dbh
 ## As long as you have big trees in open habitat, good. 
 
 ## tree ht vs. habitat
-habitat_treeht <- ggplot(anco, aes(x=habitat2,y=ht)) + xlab("Habitat Type") + 
+habitat_treeht <- ggplot(anco, aes(x=habitat,y=ht)) + xlab("Habitat Type") +
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 habitat_treeht
 
 ## habitat vs. nestht
-habitat_nestht <- ggplot(anco, aes(x=habitat2,y=nestht)) + xlab("Habitat Type") + 
+habitat_nestht <- ggplot(anco, aes(x=habitat,y=nestht)) + xlab("Habitat Type") + 
   geom_point(col="darkgreen", size=3) + theme_bw()
 habitat_nestht
 
-hab_nesthtbox <- ggplot(anco, aes(x=habitat2,y=nestht)) + xlab("Habitat Type") +
+hab_nesthtbox <- ggplot(anco, aes(x=habitat,y=nestht)) + xlab("Habitat Type") +
   ylab("Nest Height") +  geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 hab_nesthtbox ## Check outlier
 
@@ -84,11 +89,6 @@ hab_nesthtbox ## Check outlier
 dbh_building <- ggplot(anco, aes(x=Building,y=dbh)) + xlab("Distance to habitation") +
   geom_point(col="darkgreen", size=2) + theme_bw()
 dbh_building
-
-## Polygon of dbh vs. habitation
-# dbh_habpoly <- ggplot(anco, aes(x=Building,y=dbh)) + xlab("Distance to habitation") +
-#  coord_flip() +  geom_polygon(col="darkgreen", size=1) + theme_bw()
-# dbh_habpoly
 
 ## Nest tree sps vs. height
 sps_treeht_box <- ggplot(anco, aes(x=Species,y=ht)) + xlab("Tree Species") + 
@@ -104,11 +104,6 @@ sps_nestht
 vegetation <- ggplot(anco, aes(x=habitat,y=dbh)) + geom_boxplot(col="darkgreen") + theme_bw()
 vegetation
 
-####
-box_anco<-ggplot(m.anco, aes(x=habitat,y=nestht)) +
-    geom_boxplot() + theme_bw()
-box_anco
-
 ## Statistical Analyses
 ## Nest trees have similar characteristics as in the forest, and they seem to "find" them.
 ## 
@@ -116,28 +111,24 @@ box_anco
 # Test for normality- shapiro.test()
 shapiro.test(anco$dbh)
 shapiro.test(anco$ht)
-shapiro.test(anco$nestht)
+shapiro.test(anco$nestht) # not normal
 shapiro.test(anco$Stream) # Not normal
 
-#### TO DO- refine distances so that they're not right censored! #######
 ## Test for correlations between variables.
-cor.test(anco$Stream, anco$Road, method="pearson")
+cor.test(anco$Building, anco$Road, method="pearson")
 
 # Two sample t test for distances
-t.test(x=anco$Building[anco$habitat2=="Open"], y=anco$Building[anco$habitat2=="Forest"])
+t.test(x=anco$Building[anco$habitat=="Open"], y=anco$Building[anco$habitat=="Forest"])
 t.test(anco$dbh[anco$Species=="Mangifera indica"], anco$dbh[anco$Species=="Terminalia bellerica"])
-t.test(x=anco$Stream[anco$habitat2=="Open"], y=anco$Stream[anco$habitat2=="Forest"])
+t.test(x=anco$Stream[anco$habitat=="Open"], y=anco$Stream[anco$habitat=="Forest"])
 t.test(x=anco$Road, y=anco$Building)
 ## Useful? What does this mean?
 ks.test(x=anco$Stream, y=anco$Building)
 
 # Trying linear models to look at what affects nest choice
-anco.lm <- lm(hab_binary ~ Stream + Building + nestht + dbh, anco)
+anco.lm <- lm( ~ Stream + Building + nestht + dbh, anco)
 anova(anco.lm)
 
-anco.lm2 <- lm(dbh ~ Species + nestht + ht, anco)
-anova(anco.lm2)
-
-pca_anco <- prcomp(~dbh + ht + nestht + Stream + Road + Building + hab_binary, data=anco, scale=T)
-plot(pca_anco$x[,1], pca_anco$x[,2], cex=0.5, pch=16, xlab="PC1", ylab="PC2")
-text(pca_anco$x[,1], pca_anco$x[,2], labels=(anco$Stream), pos=2, offset=0.3, cex=0.6)
+pca_anco <- prcomp(~dbh + ht + nestht + Stream + Road + Building, data=anco, scale=T)
+plot(pca_anco$x[,1], pca_anco$x[,2], cex=0.5, pch=16, xlab="PC1", ylab="PC2", xlim=c(-4,5))
+text(pca_anco$x[,1], pca_anco$x[,2], labels=(anco$Number), pos=2, offset=0.3, cex=0.6)
