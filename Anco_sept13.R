8df0713023dfec381fd601a3d26c05e032408b4c
nushiamme <nushiamme@gmail.com>
2013-12-03 22:32:35 -0500

    Added nest direction stuff to compare to Mudappa (1997)

--- Anco_sept13.R
+++ Anco_sept13.R
@@ -14,7 +14,7 @@ library(raster)
 library(rgdal)
 
 ## Set working directory
-setwd("C://Users//Anusha/Desktop/R scripts/hornbills")
+setwd("C://Users//Anusha/Documents/Dropbox/Hornbills/")
 
 ## Read in data
 #anco2 <- read.csv("Ancodata_Sept2013.csv")
@@ -27,7 +27,20 @@ ebird_old <- read.csv("ebird_old.csv")
 ## Cleaning and aggregating data
 
 ## Subset necessary columns
-anco <- anco[,c(1,2,5,6,7,8,9,11,12,13,17,21,22,23,24)]
+anco <- anco[,c(1,2,5,6,7,8,9,11,12,13,14,17,21,22,23,24)]
+
+# Change direction N-NW to NW
+#anco$Direction[anco$Direction=="N-NW"] <- "NW"
+
+## Loop to count number of nests in each direction
+nest.directions <- 0
+direc <- function(dat) {
+for (i in levels(factor(dat))) {
+  nest.directions[i] <- length(dat[dat==i])
+  }
+return(nest.directions)
+}
+nest.directions <- direc(anco$Direction)
 
 ##### Sort dbh's into size classes. ###### 
 #Note: Sort dataset by increasing dbh before doing this.
@@ -64,25 +77,28 @@ sps_dbh <- ggplot(anco, aes(x=Species,y=dbh)) + xlab("Tree Species") +
   theme(axis.text.x=element_text(angle=60, vjust=1, hjust=1))
 sps_dbh
 
-## Habitat vs. distance from building
+## Habitat vs. distance from building --------- Significant
 habitat_building_box <- ggplot(anco, aes(x=habitat,y=Building)) + xlab("Habitat Type") + 
-  geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
+  geom_boxplot(col="darkgreen", size=0.3) + theme_bw() + ggtitle("Distance to building by Habitat type") +
+  theme(plot.title = element_text(size=20)) +
+  theme(axis.title.x = element_text(size = rel(1.5))) + theme(axis.title.y = element_text(size = rel(1.5))) +
+  theme(axis.text.x = element_text(size=13)) + theme(axis.text.y = element_text(size=13))
 habitat_building_box
 
 ## Habitat vs. distance from stream
 habitat_stream <- ggplot(anco, aes(x=habitat,y=Stream)) + xlab("Habitat Type") + 
-  theme_bw() + geom_point() # geom_boxplot(col="darkgreen", size=0.3)
+  theme_bw() + geom_boxplot() # geom_boxplot(col="darkgreen", size=0.3)
 habitat_stream
 
 ## Comparing building and stream distances vs habitat. 
 # Need to add legend- red is building, blue is stream
-habitat_building <- ggplot(anco, aes(x=habitat,y=Building)) + xlab("Habitat") +
-  ylab("Distance") + geom_point(col="red", size=3) + theme_bw()
-habitat_building + geom_point(data=anco, aes(x = habitat, y = Stream), col="blue", size=3)
+#habitat_building <- ggplot(anco, aes(x=habitat,y=Building)) + xlab("Habitat") +
+#  ylab("Distance") + geom_point(col="red", size=3) + theme_bw()
+#habitat_building + geom_point(data=anco, aes(x = habitat, y = Stream), col="blue", size=3)
 
 ## dbh vs. habitat
 habitat_dbh <- ggplot(anco, aes(x=habitat,y=dbh)) + xlab("Habitat Type") + 
-  geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
+  geom_boxplot(col="darkgreen", size=0.3) + theme_bw() + ggtitle("dbh by Habitat Type")
 habitat_dbh
 ## As long as you have big trees in open habitat, good. 
 
@@ -106,7 +122,7 @@ dbh_building <- ggplot(anco, aes(x=Building,y=dbh)) + xlab("Distance to habitati
 dbh_building
 
 ### dbh vs. distance to Forest
-dbh_forest <- ggplot(anco, aes(x=dbhcl,y=Forest)) + 
+dbh_forest <- ggplot(anco, aes(x=dbhcl,y=Forest)) + #stat_smooth(method='lm') +
   geom_bar(stat="identity") + theme_bw()
 dbh_forest
 
@@ -115,14 +131,14 @@ sps_treeht_box <- ggplot(anco, aes(x=Species,y=ht)) + xlab("Tree Species") +
   geom_boxplot(col="darkgreen", size=0.3) + theme_bw()
 sps_treeht_box
 
-## Nest tree sps vs. nest height
+## Nest tree sps vs. nest height ------ Interesting
 sps_nestht <- ggplot(anco, aes(x=Species,y=nestht)) + xlab("Tree Species") + 
   geom_boxplot(col="darkgreen", size=.3) + theme_bw()
 sps_nestht
 
-## Habitat type vs. dbh
-vegetation <- ggplot(anco, aes(x=habitat,y=dbh)) + geom_boxplot(col="darkgreen") + theme_bw()
-vegetation
+## Nest cavity direction
+direc <- ggplot(anco, aes(Direction)) + geom_bar() + theme_bw()
+direc
 
 ## Statistical Analyses
 ## Nest trees have similar characteristics as in the forest, and they seem to "find" them.
@@ -138,10 +154,11 @@ shapiro.test(anco$Stream) # Not normal
 cor.test(anco$Building, anco$Road, method="pearson")
 
 # Two sample t test for distances
-t.test(x=anco$Building[anco$habitat=="Open"], y=anco$Building[anco$habitat=="Forest"])
+t.test(x=anco$Building[anco$habitat=="Open"], y=anco$Building[anco$habitat=="Forest"]) ## Significant
 t.test(anco$dbh[anco$Species=="Mangifera indica"], anco$dbh[anco$Species=="Terminalia bellerica"])
 t.test(x=anco$Stream[anco$habitat=="Open"], y=anco$Stream[anco$habitat=="Forest"])
 t.test(x=anco$Road, y=anco$Building)
+cor.test(anco$nestht,anco$Forest)
 
 # Trying linear models to look at what affects nest choice
 anco.lm <- lm(dbh ~ Stream + Building + Forest + Elevation_m, anco)
@@ -152,7 +169,14 @@ pca_anco <- prcomp(~dbh + Stream + Building + Forest + sp_num, data=anco, scale=
 plot(pca_anco$x[,1], pca_anco$x[,2], cex=0.5, pch=16, xlab="PC1", ylab="PC2", xlim=c(-4,5))
 text(pca_anco$x[,1], pca_anco$x[,2], labels=(anco$Number), pos=2, offset=0.3, cex=0.6)
 
-## Mapping points
+# Trying poisson for orientation
+chisq.test(nest.directions)
+x.poi<-rpois(n=200,lambda=2.5) 
+hist(x.poi,main="Poisson distribution") 
+
+
+
+### ------------- Mapping points
 ## Function to convert degree minute seconds to decimal degrees
 convert <-function(coord){
   tmp1 <- strsplit(coord,"°")
@@ -189,7 +213,7 @@ nestpts
 ancomap <- qmap("Devrukh, India", zoom = 11, legend = "bottomleft", color="color", maptype="hybrid")
 nest_pts <- ancomap +
   geom_point(aes(x = lon, y = lat),
-             data = anco, size=4)
+             data = anco, size=4, col='red')
 nest_pts
 
 # qmap 2. for bw
