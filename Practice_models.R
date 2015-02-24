f2dac1864c69c1847584082ee9caed228b4c3047
Anusha Shankar <nushiamme@gmail.com>
2015-02-24 01:35:34 -0500

    Modified script slightly

--- Practice_models.R
+++ Practice_models.R
@@ -3,12 +3,11 @@ y <- plnorm(x)
 y <- exp(-(((x-mean(x))^2)/(2*((sd(x))^2))))
 
 plot(y~x)
-i
 
 ## From Jon Borelli
 a <- rnorm(100)
 b <- rnorm(100)
-mod1 <- lm(a~b,contrasts = )
+mod1 <- lm(a~b)
 summary(mod1)
 
 hist(mod1$residuals)
