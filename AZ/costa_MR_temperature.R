91a4776ecf4286920f15213149e4f5598d5ec042
Anusha Shankar <nushiamme@gmail.com>
2015-03-03 10:16:18 -0500

    Adjusting aspect ratio of budget plot

--- AZ/costa_MR_temperature.R
+++ AZ/costa_MR_temperature.R
@@ -59,11 +59,12 @@ hist(below.glm$residuals)
 
 ## Plot Time/Energy budgets from Pearson 1954 data
 m.budget$value <- as.numeric(m.budget$value)
-ggplot(m.budget, aes(variable, value, fill=Activity)) + xlab("Type of budget") + ylab("Percentage") + 
+budget_plot <- ggplot(m.budget, aes(variable, value, fill=Activity)) + xlab("Type of budget") + ylab("Percentage") + 
     geom_bar(stat="identity") + theme_bw() +
   theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
-       axis.text.x = element_text(size=12, face="bold"),
-       axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12),
-        legend.title = element_text(size=16), legend.text = element_text(size = 16)) +
+        axis.text.x = element_text(size=13, face="bold"),
+       axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=13),
+        legend.title = element_text(size=16), legend.text = element_text(size = 12, face="bold")) +
   scale_x_discrete(labels = function(variable) str_wrap(variable, width = 14)) + 
   scale_fill_brewer(type = "qual",palette = 6,breaks=c("Torpor/Sleep","Flying","Perching"))
+budget_plot
