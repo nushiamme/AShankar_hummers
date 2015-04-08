2d63c8228c7fab7c53150cb8e14e95f2be139160
Anusha Shankar <nushiamme@gmail.com>
2015-04-08 12:11:47 -0400

    Added plot for energy and hours in torpor

--- Torpor_paper.R
+++ Torpor_paper.R
@@ -1,12 +1,35 @@
 ## March 16, 2015
 ## Torpor paper, A. Shankar, R. Schroeder et al.
 library(ggplot2)
+library(reshape)
 
-data <- data.frame (id=1:3, weight=c(20,27,24))
-torpor <- data.frame(hc=c(2,2,4,5,6,6,6,NA))
-torpor$sc <- c(0,0,0,1,1,2,4,4)
+## setwd and read in file
+setwd("C:\\Users\\ANUSHA\\Dropbox\\Hummingbird energetics\\Arizona Torpor")
+torpor <- read.csv("Torpor_summary.csv")
 
-m.tor <- melt(torpor, measure.vars = c("hc", "sc"))
-names(m.tor) <- c("Site", "Hours")
-ggplot(m.tor, aes(x = Site, y = Hours)) + theme_bw() + geom_boxplot() + 
-  geom_point(aes(col = Site, pch = 18))
+## Rename columns, remove spaces
+names(torpor) <- c("Site", "Bird_no", "Nighttime_Energy", "Energy", "Hours_torpor")
+
+tor <- data.frame(torpor$Site, torpor$Energy, torpor$Hours_torpor)
+names(tor) <- c("Site", "Energy", "Hours_torpor")
+
+energy_plot <- ggplot(tor, aes(Site,Energy)) + theme_bw() + geom_boxplot() + 
+  geom_point(aes(col=Site, size=2)) + scale_size(guide = 'none') + scale_shape_identity() +
+  scale_color_manual(values = c("grey60", "black"), guide = FALSE) + ylab("Points") + 
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12))
+hours_torpor_plot <- ggplot(tor, aes(Site, Hours_torpor)) + theme_bw() + geom_boxplot() +
+  geom_point(aes(col=Site, size=2)) + scale_size(guide = 'none') + scale_shape_identity() +
+  scale_color_manual(values = c("grey60", "black"), guide = FALSE) + 
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12))
+grid.arrange(energy_plot, hours_torpor_plot, nrow=1, ncol=2)
+
+## Melt
+#m.tor <- melt(torpor, id.vars = "Site", measure.vars = c("Energy", "Hours_torpor"))
+#ggtor<- ggplot(m.tor, aes(Site, as.numeric(value))) + theme_bw() + facet_grid(~variable) + 
+#  geom_boxplot() + geom_point(aes(col = Site, pch = 18)) + scale_shape_identity()
+
+
