babe3043423aa5b471c7135c29a9cfd4e08ba5af
Anusha Shankar <nushiamme@gmail.com>
2015-04-29 19:27:40 -0400

    Adding plots

--- AZ/AZ_Temperature.R
+++ AZ/AZ_Temperature.R
@@ -10,3 +10,17 @@ setwd("C://Users//ANUSHA//Dropbox//Hummingbird energetics//AZ temperature 2")
 ## Ambient temperature for Harshaw Creek, 6/27/13 - 6/28/13 and Sonoita Creek 7/2/2013 - 7/3/2013
 AZ_ta <- read.csv("AZ_Temp_toPlot.csv")
 
+##Melt AZ_ta
+m.AZta <- melt(AZ_ta, id.vars = c("Site", "Date", "Time"), measure.vars = c("Min_Ta", "Max_Ta", "Average"))
+
+m.Mean <- m.AZta[m.AZta$variable=="Average",]
+m.Max <- m.AZta[m.AZta$variable=="Max_Ta",]
+m.Min <- m.AZta[m.AZta$variable=="Min_Ta",]
+
+## Plots
+AZ_TaPlot <- ggplot(m.Max, aes(as.numeric(Time), value)) + stat_smooth() + geom_point() + theme_bw() + facet_grid(.~Site)
+AZ_TaPlot2 <- ggplot(m.Max, aes(as.numeric(Time), value)) + geom_point() + theme_bw() + facet_grid(.~Site)
+AZ_TaPlot2
+AZ_TaPlot
+
+#geom_smooth(model=lm, aes(group=1))
