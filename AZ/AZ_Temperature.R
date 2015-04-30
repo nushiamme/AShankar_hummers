25f65b10ad78bfb7aa8734edcde5a655e1a60986
Anusha Shankar <nushiamme@gmail.com>
2015-04-30 13:44:50 -0400

    Changing plot aesthetics

--- AZ/AZ_Temperature.R
+++ AZ/AZ_Temperature.R
@@ -25,22 +25,21 @@ AZ_TaPlot <- ggplot(m.Mean, aes(as.numeric(Time), value)) + stat_smooth() +
   scale_x_discrete(breaks=c("0", "500", "1000", "1500", "2000"),
                    labels=c("00:00", "05:00", "10:00", "15:00", "20:00")) + 
   theme_bw() + facet_wrap(Date~Site)
-AZ_TaPlot + geom_step(subset=.(Date=="6/27/2013"))
+AZ_TaPlot
 
-Ta_HC <- ggplot(m.Mean[m.Mean$Date==c("6/27/2013","6/28/2013"),], aes(as.numeric(Time), value)) +
-  stat_smooth() + geom_point() + xlab ("Time") + 
+Ta_HC <- ggplot(m.Mean[m.Mean$Date==c("6/27/2013","6/28/2013"),], 
+                aes(as.numeric(Time), value)) + stat_smooth() + geom_point(colour="red", size=3) + xlab ("Time") + 
   scale_x_discrete(breaks=c("0", "500", "1000", "1500", "2000"),
                    labels=c("00:00", "05:00", "10:00", "15:00", "20:00")) + 
-  theme_bw() + facet_grid(Site~Date)
+  theme_bw() + facet_grid(Site~Date) + theme(legend.position = "none")
 Ta_HC
 
-Ta_SC <- ggplot(m.Mean[m.Mean$Date==c("7/2/2013","7/3/2013"),], aes(as.numeric(Time), value)) + 
-  stat_smooth() + geom_point() + xlab ("Time") + 
+Ta_SC <- ggplot(m.Mean[m.Mean$Date==c("7/2/2013","7/3/2013"),], 
+                aes(as.numeric(Time), value)) + stat_smooth() + geom_point(colour="black",size=3) + xlab ("Time") + 
   scale_x_discrete(breaks=c("0", "500", "1000", "1500", "2000"),
                    labels=c("00:00", "05:00", "10:00", "15:00", "20:00")) + 
-  theme_bw() + facet_grid(Site~Date)
+  theme_bw() + facet_grid(Site~Date) + theme(legend.position = "none")
 Ta_SC
-
 
 grid.arrange(Ta_HC, Ta_SC, nrow=2)
 #geom_smooth(model=lm, aes(group=1))
