122fa98c20df6655a2870835af92db71a4c463e6
Anusha Shankar <nushiamme@gmail.com>
2015-08-17 02:26:58 -0500

    Starting code to analyze temp data

---
+++ Ecuador_2015/Temperature_data_2015.R
@@ -0,0 +1,8 @@
+## August 16, 2015
+## Anusha Shankar
+## El Gullan, Ecuador, June 2015 - January 2016
+## iButton temperature analyses
+
+library(reshape)
+
+setwd("C:\\Users\\ANUSHA\\Dropbox\\Data 2015\\Behavior")
