918047dd033454392b77ef91ef8bad1a72d5a8a7
Anusha Shankar <nushiamme@gmail.com>
2015-11-13 13:56:39 -0500

    Updating code for November behavior

--- Ecuador_2015/Hummer_behavior.R
+++ Ecuador_2015/Hummer_behavior.R
@@ -18,15 +18,24 @@ m.beh <- melt(beh, id.vars = c("Observer", "Time", "Species", "Month"),
 m.beh_aug <- melt(beh_aug, id.vars = c("Observer", "Time", "Species", "Month"), 
               measure.vars = c("FL", "Hov", "Perch"), na.rm = T)
 
-##Checking old file
-Total_FL_aug <- sum(m.beh_aug$value[m.beh_aug$variable=="FL"]) # adding total spent flying
-Total_Hov_aug <- sum(m.beh_aug$value[m.beh_aug$variable=="Hov"])
-Total_perch_aug <- sum(m.beh_aug$value[m.beh_aug$variable=="Perch"])
+m.beh_mainsp <- m.beh[m.beh$Species==c("AGCU","COIR","METY","HEVI"),]
 
+##Checking old file
+Total_FL_aug <- sum(as.numeric(m.beh_aug$value[m.beh_aug$variable=="FL"])) # adding total spent flying
+Total_Hov_aug <- sum(as.numeric(m.beh_aug$value[m.beh_aug$variable=="Hov"]))
+Total_perch_aug <- sum(as.numeric(m.beh_aug$value[m.beh_aug$variable=="Perch"]))
 
 Total_FL <- sum(as.numeric(m.beh$value[m.beh$variable=="FL"])) # adding total spent flying
 Total_Hov <- sum(as.numeric(m.beh$value[m.beh$variable=="Hov"]))
 Total_perch <- sum(as.numeric(m.beh$value[m.beh$variable=="Perch"]))
+
+total_beh_time <- sum(Total_FL,Total_Hov,Total_perch)
+Total_FL_perc <- (Total_FL/total_beh_time)*100
+Total_Hov_perc <- (Total_Hov/total_beh_time)*100
+Total_perch_perc <- (Total_perch/total_beh_time)*100
+Total_FL_perc
+Total_Hov_perc
+Total_perch_perc
 
 Fl_not_1 <- m.beh$variable=="FL"[m.beh$Beh_no!=1&m.beh$variable=="FL"]
 length(Fl_not_1)
@@ -34,5 +43,5 @@ Hov_not_1 <- m.beh$variable=="Hov"[m.beh$Beh_no!=1]
 length(Hov_not_1)
 
 ## Plots
-ggplot(m.beh_aug, aes(variable, value)) + geom_bar(stat="identity") + theme_bw() ##+ 
-#  facet_grid(Observer~Month)
+beh_by_month <- ggplot(m.beh, aes(variable, value)) + geom_bar(stat="identity") + theme_bw() +
+  facet_grid(~Month)
