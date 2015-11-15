e65d4af4ae706520a74ca9635e6076c4dba81eaa
Anusha Shankar <nushiamme@gmail.com>
2015-11-15 17:38:39 -0500

    New plots with updated files from Torpor table

--- Torpor_paper_Nov2015.R
+++ Torpor_paper_Nov2015.R
@@ -11,13 +11,15 @@ torpor <- read.csv("Torpor_table_plot.csv")
 names(torpor)
 
 m.tor <- melt(torpor, id.vars = c("Site","Species","Day","Month","Year","Daytime_Ta_mean_C"), 
-              measure.vars = "NEE_kJ")
+              measure.vars = c("NEE_kJ","Hours_torpid"))
 ##"Time_of_entry","Hours_torpid","Hours_normo","Nighttime_Ta_mean_C","Tc_mean_C",
 ##"Min_EE_torpid","Min_EE_normo",
+##m.NEE <- m.tor[m.tor$variable=="NEE_kJ",]
 
-energy_plot <- ggplot(m.tor, aes(Species,value)) + 
-  theme_bw() + geom_boxplot() +   geom_point(aes(col=Site, size=2)) + scale_size(guide = 'none') + 
-  scale_shape_identity() +  #scale_color_manual(values = c("grey60", "black"), guide = FALSE) + 
+energy_plot <- ggplot(torpor, aes(Species, NEE_kJ)) + theme_bw() + geom_boxplot() + 
+  facet_grid(Site~.) + geom_line(aes(m.tor[m.tor$variable=="Hours_torpid",])) +
+  #scale_size(guide = 'none') + 
+  #geom_point(aes(col=Site, size=2)) + scale_shape_identity() + scale_color_manual(values = c("grey60", "black"), guide = FALSE) + 
   ylab("Nighttime energy expenditure (kJ)") + 
   theme(axis.title.x = element_text(size=16, face="bold"),
         axis.text.x = element_text(size=14),
