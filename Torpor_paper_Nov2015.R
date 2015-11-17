7251c0679ca8b8b58159aaee4edab738e38bb802
Anusha Shankar <nushiamme@gmail.com>
2015-11-17 16:33:33 -0500

    Adding to graphs

--- Torpor_paper_Nov2015.R
+++ Torpor_paper_Nov2015.R
@@ -8,7 +8,7 @@ library(gridExtra)
 
 ## setwd and read in file
 setwd("C:\\Users\\ANUSHA\\Dropbox\\Hummingbird energetics\\Tables_for_paper")
-torpor <- read.csv("Torpor_table_plot.csv")
+torpor <- read.csv("Torpor_table_plot2.csv")
 names(torpor)
 
 m.tor <- melt(torpor, id.vars = c("Site","Species","Day","Month","Year","Daytime_Ta_mean_C"), 
@@ -20,14 +20,10 @@ m.tor <- melt(torpor, id.vars = c("Site","Species","Day","Month","Year","Daytime
 # Line to arrange Site facets in sensible order
 torpor$Site_new = factor(torpor$Site, levels=c('HC','SC','SWRS','MQ','SL'))
 
-give.n <- function(Species){
-  return(c(y = mean(Species), label = length(Species)))
-}
-
 ## Plot for Nighttime energy expenditure, by species
 energy_plot <- ggplot(torpor, aes(Species, NEE_kJ)) +  theme_bw() +
-  geom_boxplot() + facet_grid(.~Site_new, scale="free_x", space="free") + 
-  ylab("Nighttime energy expenditure (kJ)") + 
+  geom_boxplot(aes(col=Species)) + facet_grid(.~Site_new, scale="free_x", space="free") + 
+  ylab("Nighttime energy expenditure (kJ)") + theme(legend.position="none") +
   theme(axis.title.x = element_text(size=16, face="bold"),
         axis.text.x = element_text(size=14),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14)) +
@@ -36,11 +32,12 @@ energy_plot
 
 ## Plot for hours spent torpid
 hours_plot <- ggplot(torpor, aes(Species, Hours_torpid)) +  theme_bw() +
-  geom_boxplot() + facet_grid(.~Site_new, scale="free_x", space="free") + 
-  ylab("Hours Torpid") + 
+  geom_boxplot(aes(col=Species)) + facet_grid(.~Site_new, scale="free_x", space="free") + 
+  ylab("Hours Torpid") + theme(legend.position="none") +
   theme(axis.title.x = element_text(size=16, face="bold"),
         axis.text.x = element_text(size=14),
-        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14))
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14)) +
+  stat_summary(fun.data = give.n, geom = "text")
 hours_plot
 grid.arrange(energy_plot, hours_plot, nrow=1, ncol=2)
 
@@ -57,6 +54,7 @@ energy_temp
 ## NEE plot by chamber temperature, facet by site and color by species
 energy_temp2 <- ggplot(torpor, aes(Tc_mean_C, NEE_kJ)) +  theme_bw() +
   geom_point(aes(col=Species), size=3) + scale_color_brewer(palette = "Set1") +
+  stat_summary(fun.data = give.n, geom = "text")
   #facet_grid(.~Site,space="free") + 
   ylab("Nighttime energy expenditure (kJ)") + 
   theme(axis.title.x = element_text(size=16, face="bold"),
@@ -74,6 +72,27 @@ min_normo_EE <- ggplot(torpor, aes(Tc_mean_C, Min_EE_normo)) +  theme_bw() +
         axis.text.x = element_text(size=14),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14)) 
 min_normo_EE
+
+avg_normo_EE <- ggplot(torpor, aes(Tc_mean_C, EE_per_hour_normo)) +  theme_bw() + 
+  geom_point(aes(col=Species), size=3) + theme(legend.position="none") +
+  scale_color_brewer(palette = "Set1") +
+  #facet_grid(.~Site,space="free") +
+  ylab("Avg EE normothermic") + xlab("Tc mean (C)") +
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14)) 
+avg_normo_EE
+
+avg_torpid_EE <- ggplot(torpor, aes(Tc_mean_C, EE_per_hour_torpid)) +  theme_bw() + 
+  geom_point(aes(col=Species), size=3) + #theme(legend.position="none") +
+  scale_color_brewer(palette = "Set1") +
+  #facet_grid(.~Site,space="free") +
+  ylab("Avg EE torpid") + xlab("Tc mean (C)") +
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14)) 
+avg_torpid_EE
+grid.arrange(avg_normo_EE, avg_torpid_EE, min_normo_EE, min_torpid_EE, nrow=2, ncol=2)
 
 ## Min torpid EE by Tc
 min_torpid_EE <- ggplot(torpor, aes(Tc_mean_C, Min_EE_torpid)) +  theme_bw() + 
