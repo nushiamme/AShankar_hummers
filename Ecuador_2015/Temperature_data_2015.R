8bcbb0ff8410577041a6a36afa80a9e5e32cda7b
Anusha Shankar <nushiamme@gmail.com>
2016-02-04 01:29:38 -0500

    R-added august graphs for a sample plot

--- Ecuador_2015/Temperature_data_2015.R
+++ Ecuador_2015/Temperature_data_2015.R
@@ -14,12 +14,14 @@ head(Ta_TillNov)
 ##m.temp <- melt(Ta_JulAug, id.vars = c("Month", "Day", "Year", "Time", "AM_PM", "iButton_number"), 
 ##     measure.vars = "Temperature")
 
+#### TODO - have to add AM, PM and check what happened with after 9:40am
 Temp_aug <- ggplot(Ta_JulAug, aes(Time,Temperature)) + theme_classic(base_size = 30) + geom_boxplot() + 
   scale_color_discrete() + #geom_smooth(aes(group=1)) + facet_grid(~Month) + 
   theme(axis.title.x = element_text(size=16, face="bold"), 
         axis.text.x = element_text(size=12, face="bold", angle = 60, vjust=0.2, hjust = 0.4),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12))
 Temp_aug
+
 max(Ta_TillNov$Temperature)
 
 #### TODO - have to add AM, PM and check what happened with after 9:40am
