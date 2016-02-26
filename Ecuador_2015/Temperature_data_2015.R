2b36641901721d553eb5c8eed90b871b453f272c
Anusha Shankar <nushiamme@gmail.com>
2016-02-26 16:40:48 -0500

    Changed folder name, also Temperature_data_2015.R - added data from November to Jan into samecsv, and updating code and problems

--- Ecuador_2015/Temperature_data_2015.R
+++ Ecuador_2015/Temperature_data_2015.R
@@ -10,7 +10,8 @@ library(maptools)
 setwd("C:\\Users\\ANUSHA\\Dropbox\\Data 2015\\Temperature\\iButton_Ta")
 Ta_JulAug <- read.csv("CompiledTemp_July-Aug16_2015.csv")
 Ta_TillNov <- read.csv("CompiledTemp_Nov28_2015.csv")
-head(Ta_TillNov)
+Ta_all <- read.csv("CompiledTemp_All.csv")
+head(Ta_all)
 ## Weird order of times in the middle of the file, Dec 1 - Dec 9. Took those dates out
 #samp <- read.csv("Sample_ToPlot.csv")
 #samp$hms <- format(samp$Time, format = "%H:%M:%S")
@@ -24,27 +25,6 @@ samp <- read.csv("Sample2_ToPlot.csv")
 Ta.xlab <- expression(atop(paste("Ambient Temperature (", degree,"C)")))
 
 OneDay_samp <- samp[samp$Date=="11/28/2015",]
-
-### This is a horribly manual method, but just getting it done for now
-OneDay_samp$Time2 <- factor(OneDay_samp$Time2, 
-                     levels = c("0:26","0:56", "1:26","1:56", "2:26", "2:56", "3:26","3:56", "4:26",
-                                "4:56", "5:26", "5:56", "6:26", "6:56","7:26", "7:56", "8:26", "8:56",
-                                "9:26", "9:56", "10:26","10:56", "11:26", "11:56", "12:26", "12:56",
-                                "13:26","13:56", "14:26", "14:56", "15:26", "15:56", "16:26","16:56",
-                                "17:26", "17:56", "18:26", "18:56", "19:26","19:56", "20:26","20:56",
-                                "21:26","21:56", "22:26","22:56", "23:26", "23:56"))
-
-samp_plot <- ggplot(OneDay_samp, aes(Time2,Temp_C)) + theme_classic(base_size = 35) + 
-  geom_point(size=3) + #geom_text(vjust = 0, nudge_y = 0.5) +
-  scale_color_discrete() + #geom_smooth(aes(group=1)) + #facet_grid(~Month) + 
-  theme(axis.title.x = element_text(face="bold"), 
-        axis.text.x = element_text(face="bold", angle=60, vjust=0.4, hjust = 0.4),
-        axis.title.y = element_text(face="bold", vjust=-2), axis.text.y = element_text(size=35)) +
-  xlab("Time of Day") + ylab(Ta.xlab) + 
-  scale_x_discrete(breaks = c("0:26","1:56","3:26","4:56","6:26","7:56","9:26","10:56", "12:26",
-                              "13:56","15:26","16:56","18:26","19:56","21:26","22:56"))
-samp_plot
-
 
 #### TODO - have to add AM, PM and check what happened with after 9:40am
 Temp_aug <- ggplot(Ta_JulAug, aes(Time,Temperature)) + theme_classic(base_size = 30) + 
@@ -64,3 +44,32 @@ Temp_plot <- ggplot(Ta_TillNov, aes(Time,Temperature)) + theme_bw() + geom_point
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12))
 Temp_plot
 max(Ta_TillNov$Temperature)
+
+## Trying newly formatted, all data
+Temp_plot <- ggplot(Ta_all, aes(Time,Temperature)) + theme_bw() + 
+  geom_point(aes(col=as.factor(iButton_number))) + scale_color_discrete() + #geom_smooth(aes(group=1)) + 
+  facet_grid(~Month) + theme(axis.title.x = element_text(size=16, face="bold"), 
+        axis.text.x = element_text(size=12, face="bold", angle = 60, vjust=0.2, hjust = 0.4),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12))
+Temp_plot
+
+
+### This is a horribly manual method, but just getting it done for now
+OneDay_samp$Time2 <- factor(OneDay_samp$Time2, 
+                            levels = c("0:26","0:56", "1:26","1:56", "2:26", "2:56", "3:26","3:56", "4:26",
+                                       "4:56", "5:26", "5:56", "6:26", "6:56","7:26", "7:56", "8:26", "8:56",
+                                       "9:26", "9:56", "10:26","10:56", "11:26", "11:56", "12:26", "12:56",
+                                       "13:26","13:56", "14:26", "14:56", "15:26", "15:56", "16:26","16:56",
+                                       "17:26", "17:56", "18:26", "18:56", "19:26","19:56", "20:26","20:56",
+                                       "21:26","21:56", "22:26","22:56", "23:26", "23:56"))
+
+samp_plot <- ggplot(OneDay_samp, aes(Time2,Temp_C)) + theme_classic(base_size = 35) + 
+  geom_point(size=3) + #geom_text(vjust = 0, nudge_y = 0.5) +
+  scale_color_discrete() + #geom_smooth(aes(group=1)) + #facet_grid(~Month) + 
+  theme(axis.title.x = element_text(face="bold"), 
+        axis.text.x = element_text(face="bold", angle=60, vjust=0.4, hjust = 0.4),
+        axis.title.y = element_text(face="bold", vjust=-2), axis.text.y = element_text(size=35)) +
+  xlab("Time of Day") + ylab(Ta.xlab) + 
+  scale_x_discrete(breaks = c("0:26","1:56","3:26","4:56","6:26","7:56","9:26","10:56", "12:26",
+                              "13:56","15:26","16:56","18:26","19:56","21:26","22:56"))
+samp_plot
