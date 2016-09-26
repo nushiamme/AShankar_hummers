87641a34ea565575408deecf1e6deab9cebb151f
Anusha Shankar <nushiamme@gmail.com>
2016-09-26 12:05:31 -0400

    Finally close to ordering plots by hour

--- TemperaturePlots2016.R
+++ TemperaturePlots2016.R
@@ -21,14 +21,29 @@ tatc$Hour_rounded <- factor(tatc$Hour_rounded,
                             levels= c("1900", "1930", "2000", "2030", "2130", "2200", "2230", "2300", "2330", "2400",
                                       "2430", "100", "130", "200", "230", "300", "330", "400", "430", "500", "530",
                                       "600", "630", "700"), ordered=T)
-aggregate()
+## Aggregate means, min and max
+Tc_mean <- aggregate(tatc$Tc_Mean, 
+                           by=list(tatc$Site, tatc$Hour_rounded), FUN="mean")
+Tc_min <- aggregate(tatc$Tc_min, 
+                              by=list(tatc$Site, tatc$Hour_rounded), FUN="min")
+Tc_max <- aggregate(tatc$Tc_max, 
+                              by=list(tatc$Site, tatc$Hour_rounded), FUN="max")
+
+tatc_summ <- merge(Tc_mean, Tc_min, 
+                       by=c("Group.1", "Group.2"))
+tatc_summ <- merge(tatc_summ, Tc_max, 
+                       by=c("Group.1", "Group.2"))
+
+names(tatc_summ) <- c("Site", "Hour_rounded", "Mean_Tc", "Min_Tc", "Max_Tc")
+
 
 ## Plots
-ggplot(tatc, aes(as.character(Hour_rounded),Tc_Mean)) + my_theme + 
+ggplot(tatc_summ, aes(Hour_rounded,Mean_Tc)) + my_theme + 
   #geom_point(aes(col=Site)) + 
-  geom_line(aes(col=Site, group=Site)) +
-  #geom_errorbar(aes(ymin=Tc_min, ymax=Tc_max), width=.1, position=pd) +
-  theme(axis.text.x = element_text(angle = 90))
+  geom_line(aes(col=Site, group=Site), size=3) +
+  geom_errorbar(aes(ymin= Min_Tc, ymax= Max_Tc, col=Site), width=.1, position=pd) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ylab("Mean chamber temperature")
 
 ggplot(tatc, aes(Hour,Ta_Mean)) + my_theme + 
   geom_point(aes(col=Site)) + geom_line(aes(col=Site, group=Site)) +
