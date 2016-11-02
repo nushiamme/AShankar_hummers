39b5b2d3b3e53ae8008c05f8c6f7e484cecf9994
Anusha Shankar <nushiamme@gmail.com>
2016-11-02 19:31:46 -0400

    Improved aesthetics on plots, and pulled out just a single day of temps, each for ambient and operative

--- Temp_EnergyBudget.R
+++ Temp_EnergyBudget.R
@@ -24,23 +24,32 @@ names(temp_details) <- c("Site", "Day", "Month", "Year", "Time", "Hour", "HC_te1
 m.te_det <- melt(temp_details, id.vars=c("Site", "Day", "Month", "Year", "Hour"), 
                measure.vars=c("HC_te1", "HC_te2", "Mixed_te1", "Mixed_te2"), na.rm=T)
 m.te_det$DayMonth <- paste0(m.te_det$Day, ",", m.te_det$Month)
-names(m.te_det) <- c("Site", "Day", "Month", "Year", "Hour", "Sensor", "Te")
+names(m.te_det) <- c("Site", "Day", "Month", "Year", "Hour", "Sensor", "Te", "DayMonth")
 
 m.ta_det <- melt(temp_details, id.vars=c("Site", "Day", "Month", "Year", "Hour"), 
                measure.vars=c("SC_ta1", "SC_ta2", "SC_ta3", "Mixed_ta1", 
                               "Mixed_ta2", "Mixed_ta3", "Mixed_ta4", "Mixed_ta5", "Mixed_ta6", "Mixed_ta7", "Mixed_ta8",
                               "Mixed_ta9", "Mixed_ta10", "Mixed_ta11", "Mixed_ta12", "Mixed_ta13"), na.rm=T)
 m.ta_det$DayMonth <- paste0(m.ta_det$Day, ",", m.ta_det$Month)
-names(m.ta_det) <- c("Site", "Day", "Month", "Year", "Hour", "Sensor", "Ta")
+names(m.ta_det) <- c("Site", "Day", "Month", "Year", "Hour", "Sensor", "Ta", "DayMonth")
 
 #### General functions ####
 my_theme <- theme_classic(base_size = 30) + 
   theme(panel.border = element_rect(colour = "black", fill=NA))
 
-#### Plots #####
-m.te_hour <- m.te_det[m.te_det$Hour==700,]
-ggplot(m.te_hour, aes(Sensor, Te)) + geom_point() + my_theme #+ facet_grid(DayMonth~.)
+Ta.lab <- expression(atop(paste("Ambient Temperature ( ", degree,"C)")))
+Te.lab <- expression(atop(paste("Operative Temperature ( ", degree,"C)")))
 
+#### Plots #####
+m.te_hour <- m.te_det[m.te_det$Hour==700 & m.te_det$DayMonth=="8,7" & m.te_det$Site=="HC",]
+ggplot(m.te_hour, aes(Sensor, Te)) + geom_point(size=4) + my_theme + 
+  ylab(Te.lab) +
+  ggtitle("Harshaw July 8, 2016, 7am")
+
+m.ta_hour <- m.ta_det[m.ta_det$Hour==700 & m.ta_det$DayMonth=="8,7" & m.ta_det$Site=="HC",]
+ggplot(m.ta_hour, aes(Sensor, Ta)) + geom_point(size=4) + my_theme + 
+  ylab(Ta.lab) +
+  ggtitle("Harshaw July 8, 2016, 7am")
 
 ggplot(m.temp[m.temp$Site=="HC",], aes(Sensor, Temp)) + geom_point() + my_theme 
   facet_grid(Hour~.)
