c12cedeca387ce9cd918064f013b5545d78af1aa
Anusha Shankar <nushiamme@gmail.com>
2016-11-07 11:09:58 -0500

    Updating mysteriously untracked files

---
+++ AShankar_hummers/Ecuador_2015/Nectar_2015.R
@@ -0,0 +1,29 @@
+## October 19, 2015
+## Anusha Shankar
+## El Gullan, Ecuador, June 2015 - January 2016
+## Nectar data analyses
+
+library(reshape)
+library(ggplot2)
+library(pastecs)
+
+orgr <- read.csv("C:\\Users\\ANUSHA\\Dropbox\\Anusha_personal\\Thesis_proposal\\R_csv\\Nectar_ORGR_Oct19_2015_without55plus.csv")
+
+nectar.orgr <- melt(orgr, id.vars = c("TimeSlot", "Day", "Month"), measure.vars = "Nectar_L_mm")
+
+BRIX.orgr <- melt(orgr, id.vars = c("TimeSlot", "Day", "Month"), measure.vars = "BRIX", na.omit=T)
+
+
+plot.nec.orgr <- ggplot(nectar.orgr, aes(TimeSlot, value)) + theme_bw() + geom_boxplot() + 
+  ylab(label = "Nectar length") + scale_x_discrete(limits=c("6 - 8 am", "9 - 11 am", "12 - 2 pm",
+                                                            "3 - 5 pm", "6 - 7 pm"))
+plot.nec.orgr
+
+plot.BRIX.orgr <- ggplot(BRIX.orgr, aes(TimeSlot, value)) + theme_bw() + geom_boxplot() + 
+  ylab(label = "BRIX") + scale_x_discrete(limits=c("6 - 8 am", "9 - 11 am", "12 - 2 pm",
+                                                   "3 - 5 pm", "6 - 7 pm"))
+plot.BRIX.orgr
+
+## Basic stats using pastecs package
+stat.desc(orgr, basic=F)
+mean(orgr$Nectar_L_mm)
---
+++ AShankar_hummers/Torpor1415/Torpor_paper.R
@@ -0,0 +1,38 @@
+## March 16, 2015
+## Torpor paper, A. Shankar, R. Schroeder et al.
+
+library(ggplot2)
+library(reshape)
+library(gridExtra)
+
+## setwd and read in file
+setwd("C:\\Users\\ANUSHA\\Dropbox\\Hummingbird energetics\\Torpor\\Arizona Torpor")
+torpor <- read.csv("Torpor_summary.csv")
+
+## Rename columns, remove spaces
+names(torpor) <- c("Site", "Bird_no", "Nighttime_Energy", "Energy", "Hours_torpor")
+
+tor <- data.frame(torpor$Site, torpor$Energy, torpor$Hours_torpor)
+names(tor) <- c("Site", "Energy", "Hours_torpor")
+
+energy_plot <- ggplot(tor, aes(Site,Energy)) + theme_bw() + geom_boxplot() + 
+  geom_point(aes(col=Site, size=2)) + scale_size(guide = 'none') + scale_shape_identity() +
+  scale_color_manual(values = c("grey60", "black"), guide = FALSE) + 
+  ylab("Nighttime energy expenditure (kJ)") + 
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14))
+hours_torpor_plot <- ggplot(tor, aes(Site, Hours_torpor)) + theme_bw() + geom_boxplot() +
+  geom_point(aes(col=Site, size=2)) + scale_size(guide = 'none') + scale_shape_identity() +
+  scale_color_manual(values = c("grey60", "black"), guide = FALSE) + 
+  theme(axis.title.x = element_text(size=16, face="bold"),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=14))
+grid.arrange(energy_plot, hours_torpor_plot, nrow=1, ncol=2)
+
+## Melt
+#m.tor <- melt(torpor, id.vars = "Site", measure.vars = c("Energy", "Hours_torpor"))
+#ggtor<- ggplot(m.tor, aes(Site, as.numeric(value))) + theme_bw() + facet_grid(~variable) + 
+#  geom_boxplot() + geom_point(aes(col = Site, pch = 18)) + scale_shape_identity()
+
+
