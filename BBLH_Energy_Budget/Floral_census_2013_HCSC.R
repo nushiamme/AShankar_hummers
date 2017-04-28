3a1bd54f88b58aa20427e3c17e9136e7c3716369
nushiamme <anusha.shankar@stonybrook.edu>
2017-04-28 15:49:03 +0200

    Starting script to analyses pre- and post- monsoon in Harshaw and Sonoita

--- BBLH_Energy_Budget/Floral_census_2013_HCSC.R
+++ BBLH_Energy_Budget/Floral_census_2013_HCSC.R
@@ -3,3 +3,33 @@
 ## Started April 27, 2017
 ## For BBLH energy budget paper
 
+## Reading in packages
+library(ggplot2)
+library(reshape)
+
+## Set working directory
+setwd("C:\\Users\\shankar\\Dropbox\\Anusha Committee\\BBLH_EnergyBudget\\Tables")
+
+## Reda in file
+floral <- read.csv("FloralCensus_cropped.csv", sep=";")
+head(floral)
+
+## Saving standard theme  
+my_theme <- theme_classic(base_size = 30) + 
+  theme(axis.title.y = element_text(color = "black", vjust = 2),
+        panel.border = element_rect(colour = "black", fill=NA))
+
+## Theme with slightly smaller font
+my_theme2 <- my_theme + theme_classic(base_size = 15)
+
+## Give sample size
+give.n <- function(x){
+  return(c(y = mean(x), label = length(x)))
+}
+
+## plot of flowers per site and month
+ggplot(floral, aes(Site,(TotalFlowers/length(unique(Date))))) + geom_bar(stat="identity", width=0.5) + 
+  my_theme + facet_grid(.~Pre_post_monsoon)# + geom_text(aes(label = length(unique(Date))), stat= "count", vjust = -.5)
+  
+  geom_text(aes(length(unique(floral$Date)), vjust=4, size=10))
+
