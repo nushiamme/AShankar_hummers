1780b520ed540eeb43c5c219c9feb580f6f6cafd
Anusha Shankar <nushiamme@gmail.com>
2017-07-12 14:21:56 -0400

    Adding script to analyze SL masses from 2016 for Lewis Clark report

---
+++ Ecuador_2015/SL2016_banding_mass_analyses.R
@@ -0,0 +1,15 @@
+## Analyzing masses in Santa Lucia, Ecuador banding data for Lewis and Clark report
+## July 12, 2017
+## Code Author: Anusha Shankar
+## Contact: anusha<dot>shankar<at>stonybrook<dot>edu
+## Data collected by: Ana Morales, Gabriela Cordova, Rosalee Elting
+
+library(ggplot2)
+library(reshape)
+
+setwd("C:\\Users\\ANUSHA\\Dropbox\\Anusha_personal\\Thesis_proposal\\Summer 2015")
+band_masses <- read.csv("SL_2016_banding_data.csv")
+
+
+
+
