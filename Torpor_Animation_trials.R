3ec30393394f789db7421f387ec668f39021de82
Anusha Shankar <nushiamme@gmail.com>
2017-12-25 00:47:36 -0500

    Making gifs bigger plots and modifying axes for SICB talk

--- Torpor_Animation_trials.R
+++ Torpor_Animation_trials.R
@@ -2,7 +2,7 @@
 
 library(animation)
 #library(requireR)
-library(magick)
+#library(magick)
 library(caTools)
 library(png)
 library(gapminder)
@@ -26,6 +26,66 @@ gcb_0720$Category <- factor(gcb_0720$Category, levels=c("N","T"))
 torCol <- c("black", "red")
 names(torCol) <- levels(gcb_0720$Category)
 colScale <- scale_colour_manual(name = "Category", values = torCol)
+
+
+# Make a ggplot, but add frame=year: one image per year
+#gpminder_data <- gapminder::gapminder
+
+for (tslot in unique(gcb_0720$TimeSlot)){
+  p_temp <- subset(gcb_0720, TimeSlot==tslot)
+  gcb_gif_temp <- ggplot(p_temp, aes(Time2, EE_J, frame = Time_chunks, col=Category)) +
+    geom_path(aes(cumulative=T)) +
+    my_theme_blank + colScale + 
+    theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
+          legend.key.height=unit(3,"line"),
+          axis.line.x = element_line(colour = "grey50"),
+          axis.line.y = element_line(colour = "grey50")) +
+    ylim(0,50) + xlab("Time (seconds)") + ylab("Energy expenditure (J)")
+  gganimate(gcb_gif_temp, paste("gcb0720_",tslot,".gif"), interval=0.05, 
+            ani.width=1500, ani.height=800)
+}
+
+gcb_gif_1 <- ggplot(gcb_0720[gcb_0720$TimeSlot==c(5,6),], aes(Time2, EE_J, frame = BirdID, col=Category)) +
+  geom_path(aes(cumulative=T)) + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
+                                                        axis.line.x = element_line(colour = "grey50"),
+                                                        axis.line.y = element_line(colour = "grey50")) +
+  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
+
+gcb_gif_5 <- ggplot(gcb_0720[gcb_0720$TimeSlot==5,], aes(Time2, EE_J, frame = Time_chunks, col=Category)) +
+  geom_path(aes(cumulative=T)) + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
+                                                        axis.line.x = element_line(colour = "grey50"),
+                                                        axis.line.y = element_line(colour = "grey50")) +
+  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
+
+gcb_gif_5_6 <- ggplot(gcb_0720[gcb_0720$TimeSlot==c(5,6),], aes(Time_hour, EE_J, frame = Time_chunks, col=Category)) +
+  geom_path() + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
+                                                        axis.line.x = element_line(colour = "grey50"),
+                                                        axis.line.y = element_line(colour = "grey50")) +
+  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
+
+gcb_gif_5_6 <- ggplot(gcb_0720[gcb_0720$TimeSlot==c(5,6),], aes(SampleNo, EE_J, col=Category)) +
+  geom_path() + my_theme_blank + theme(axis.text.x = element_text(angle=30, hjust=1, size=20),
+                                       axis.line.x = element_line(colour = "grey50"),
+                                       axis.line.y = element_line(colour = "grey50")) +
+  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
+
+# Make the animation!
+gganimate(gcb_gif_5_6, interval=0.075, ani.width=1500, ani.height=800)
+
+# Save it to Gif
+gganimate(gcf_gif_5_6, "C:\Users\ANUSHA\Dropbox\Hummingbird energetics\Tables_for_paper\\gcb_0720_5_6.gif")
+
+gcb_gif_total <- ggplot(gcb_0720, aes(SampleNo, EE_J, frame = BirdID, col=Category)) +
+  geom_path(aes(cumulative=T)) +
+  my_theme_blank + colScale + 
+  theme(axis.text.x = element_text(angle=30, hjust=1, size=20),
+        legend.key.height=unit(3,"line"),
+        axis.line.x = element_line(colour = "grey50"),
+        axis.line.y = element_line(colour = "grey50")) +
+  ylim(0,50) + xlab("Time (seconds)") + ylab("Energy expenditure (J)")
+gganimate(gcb_gif_total, "gcb_0720_total.gif", interval=0.05, ani.width=1200, ani.height=600)
+
+
 
 #im.convert("*.png", output = "bm-animation1.gif")
 
@@ -62,54 +122,3 @@ saveGIF({
 
 setwd(".\\gif_files")
 shell(' "C:\\Program Files\\ImageMagick-7.0.7-Q16\\magick.exe" magick -delay 80 *.png example_1.gif')
-
-# Make a ggplot, but add frame=year: one image per year
-#gpminder_data <- gapminder::gapminder
-
-
-for (tslot in unique(gcb_0720$TimeSlot)){
-  p_temp <- subset(gcb_0720, TimeSlot==tslot)
-  gcb_gif_temp <- ggplot(p_temp, aes(SampleNo, Time2, frame = Time_chunks, col=Category)) +
-    geom_path(aes(cumulative=T)) +
-    my_theme_blank + colScale + 
-    theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
-          legend.key.height=unit(3,"line"),
-          axis.line.x = element_line(colour = "grey50"),
-          axis.line.y = element_line(colour = "grey50")) +
-    ylim(0,50) + xlab("Time (seconds)") + ylab("Energy expenditure (J)")
-  gganimate(gcb_gif_temp, paste("gcb0720_trial3_",tslot,".gif"), interval=0.05)
-}
-
-gcb_gif_1 <- ggplot(gcb_0720[gcb_0720$TimeSlot==c(5,6),], aes(SampleNo, EE_J, frame = BirdID, col=Category)) +
-  geom_path(aes(cumulative=T)) + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
-                                                        axis.line.x = element_line(colour = "grey50"),
-                                                  axis.line.y = element_line(colour = "grey50")) +
-  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
-
-gcb_gif_5 <- ggplot(gcb_0720[gcb_0720$TimeSlot==5,], aes(Time2, EE_J, frame = Time_chunks, col=Category)) +
-  geom_path(aes(cumulative=T)) + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
-                                                        axis.line.x = element_line(colour = "grey50"),
-                                                        axis.line.y = element_line(colour = "grey50")) +
-  scale_color_manual(values = c("black", "red")) + xlab("Time") #+ xlim(0,4060)
-
-gcb_gif_6 <- ggplot(gcb_0720[gcb_0720$TimeSlot==6,], aes(Time2, EE_J, frame = Time_chunks, col=Category)) +
-  geom_path(aes(cumulative=T)) + my_theme_blank + theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
-                                                        axis.line.x = element_line(colour = "grey50"),
-                                                        axis.line.y = element_line(colour = "grey50")) +
-  scale_color_manual(values = "red") + xlab("Time") #+ xlim(0,4060)
-
-# Make the animation!
-gganimate(gcb_gif_6, interval=0.075)
-
-# Save it to Gif
-gganimate(p_gif, "271_gganimate.gif")
-
-gcb_gif_total <- ggplot(gcb_0720, aes(SampleNo, EE_J, frame = BirdID, col=Category)) +
-  geom_path(aes(cumulative=T)) +
-  my_theme_blank + colScale + 
-  theme(axis.text.x = element_text(angle=60, hjust=1, size=20),
-        legend.key.height=unit(3,"line"),
-        axis.line.x = element_line(colour = "grey50"),
-        axis.line.y = element_line(colour = "grey50")) +
-  ylim(0,50) + xlab("Time (seconds)") + ylab("Energy expenditure (J)")
-gganimate(gcb_gif_total, "gcb_0720_total.gif", interval=0.05)
