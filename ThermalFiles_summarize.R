70fb546989965b9a5fc753a9e85929b3cc48868f
Anusha Shankar <nushiamme@gmail.com>
2018-06-05 17:34:27 -0700

    Successfully summarized a whole bird's night, but have to rearrange Time axis for sensibleness

--- ThermalFiles_summarize.R
+++ ThermalFiles_summarize.R
@@ -1,15 +1,25 @@
 ## Processing thermal images and video from May-Jun 2018, Southwestern Research Station
 
 library(plyr)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
 
 setwd("E:\\Google Drive\\IR_torpor_2018\\Funny_videos\\BCHU03_0530")
 
-## Manual way, per file- to then incorporate into 'apply' funcs or into a loop
-mahu_1 <- read.csv("MAHU08_0531/MAHU08_0531_2231.csv", header=F)
+## Generic plot theme
+my_theme <- theme_classic(base_size = 30) + 
+  theme(panel.border = element_rect(colour = "black", fill=NA))
 
-mahu_1_stack <- na.omit(data.frame(stack(mahu_1[1:ncol(mahu_1)])))[,1]
-length(mahu_1_stack)
-summary(mahu_1_stack)
+## Axis labels
+Temp.lab <- expression(atop(paste("Temperature (", degree,"C)")))
+
+## Manual way, to try per file- to then incorporate into 'apply' funcs or into a loop
+mahu_1 <- read.csv("MAHU08_0531/MAHU08_0531_2231.csv", header=F)
+bchu_1 <- read.csv("BCHU03_0530_2323.csv", header=F)
+bchu_1_stack <- na.omit(data.frame(stack(bchu_1[1:ncol(bchu_1)])))[,1]
+length(bchu_1_stack)
+summary(bchu_1_stack)
 
 ## Using plyr
 paths <- dir(path = "E:\\Google Drive\\IR_torpor_2018\\Funny_videos\\BCHU03_0530\\", pattern = "\\.csv$")
@@ -17,14 +27,38 @@ names(paths) <- basename(paths)
 
 ThermFiles <- lapply(paths, read.csv, header=F)
 
+## Creating a summary data frame of 
+# Can also create automatic lists of summaries: lapply(ThermFiles_na_omit[[i]], summary)
+Thermsumm <- data.frame(matrix(NA, nrow=length(ThermFiles), ncol=4))
+names(Thermsumm) <- c("Min", "Mean", "Max", "File")
+Thermsumm$File <- noquote(names(ThermFiles))
+for(i in 1:length(ThermFiles)) { 
+  ThermFiles_na_omit <- vector("list",length(ThermFiles))
+  names(ThermFiles_na_omit) <- names(ThermFiles)
+  ThermFiles_na_omit[[i]]<- na.omit(data.frame(stack(ThermFiles[[i]][1:ncol(ThermFiles[[i]])])))[1]
+  Thermsumm[i,1] <- min(ThermFiles_na_omit[[i]]$values)
+  Thermsumm[i,2] <- mean(ThermFiles_na_omit[[i]]$values)
+  Thermsumm[i,3] <- max(ThermFiles_na_omit[[i]]$values)
+  ## Splitting the file name to get IndivID, date, and time
+  Thermsumm$Indiv_ID <- 
+    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 1)
+  Thermsumm$Date <- 
+    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 2)
+  Thermsumm$Time <- 
+    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 3)
+  Thermsumm$Time <- 
+    lapply(strsplit(as.character(Thermsumm$Time), "\\."), "[", 1)
+}
 
-ThermFiles_clean <- sapply(ThermFiles, function(x) na.omit(data.frame(stack(ThermFiles[1:ncol(ThermFiles[i])]))))
+m.thermsumm <- melt(Thermsumm, id.vars=c("Indiv_ID", "Date", "Time"), 
+                    measure.vars = c("Min", "Mean","Max"))
 
-## Not tried this yet
-combined.df <- do.call(rbind , tables)
-#You can then find the mean - find which columns are numeric
+m.thermsumm$Hour <- substr(m.thermsumm$Time,1,2)
+m.thermsumm$Hour <- factor(m.thermsumm$Hour, 
+                            levels= c("19", "20", "21", "22", "23", "24", "01", "02",
+                                      "03", "04", "05", "06"), ordered=T)
 
-s <- sapply(combined.df, is.numeric)
-#find the mean of numeric variables
-
-colMeans(combined.df[s])
+ggplot(m.thermsumm, aes(as.character(Time), value)) +
+  geom_point(aes(col=variable), size=3) + my_theme +
+  theme(axis.text.x = element_text(angle=30, size=15)) +
+  ylab(Temp.lab) + xlab("Hour")
