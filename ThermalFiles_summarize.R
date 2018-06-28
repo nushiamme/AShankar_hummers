5e9938f47c5fdb95189d3e40c003973efd22e80c
Anusha Shankar <nushiamme@gmail.com>
2018-06-28 14:18:32 -0400

    Code working except for times being wraparound rather than linear chronologically

--- ThermalFiles_summarize.R
+++ ThermalFiles_summarize.R
@@ -1,11 +1,12 @@
 ## Processing thermal images and video from May-Jun 2018, Southwestern Research Station
 
+library(here)
 library(plyr)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
 
-setwd("E:\\Google Drive\\IR_torpor_2018\\Funny_videos\\BCHU03_0530")
+here() ## Sets wd
 
 ## Generic plot theme
 my_theme <- theme_classic(base_size = 30) + 
@@ -14,15 +15,11 @@ my_theme <- theme_classic(base_size = 30) +
 ## Axis labels
 Temp.lab <- expression(atop(paste("Temperature (", degree,"C)")))
 
-## Manual way, to try per file- to then incorporate into 'apply' funcs or into a loop
-mahu_1 <- read.csv("MAHU08_0531/MAHU08_0531_2231.csv", header=F)
-bchu_1 <- read.csv("BCHU03_0530_2323.csv", header=F)
-bchu_1_stack <- na.omit(data.frame(stack(bchu_1[1:ncol(bchu_1)])))[,1]
-length(bchu_1_stack)
-summary(bchu_1_stack)
+bird_id <- "BCHU03_0530"
+setwd(here(bird_id))
 
 ## Using plyr
-paths <- dir(path = "E:\\Google Drive\\IR_torpor_2018\\Funny_videos\\BCHU03_0530\\", pattern = "\\.csv$")
+paths <- dir(pattern = "\\.csv$")
 names(paths) <- basename(paths)
 
 ThermFiles <- lapply(paths, read.csv, header=F)
@@ -41,24 +38,34 @@ for(i in 1:length(ThermFiles)) {
   Thermsumm[i,3] <- max(ThermFiles_na_omit[[i]]$values)
   ## Splitting the file name to get IndivID, date, and time
   Thermsumm$Indiv_ID <- 
-    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 1)
+    unlist(lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 1))
   Thermsumm$Date <- 
-    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 2)
+    unlist(lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 2))
   Thermsumm$Time <- 
-    lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 3)
+    unlist(lapply(strsplit(as.character(Thermsumm$File), "\\_"), "[", 3))
   Thermsumm$Time <- 
-    lapply(strsplit(as.character(Thermsumm$Time), "\\."), "[", 1)
-}
-
-m.thermsumm <- melt(Thermsumm, id.vars=c("Indiv_ID", "Date", "Time"), 
+    unlist(lapply(strsplit(as.character(Thermsumm$Time), "\\."), "[", 1))
+  m.thermsumm <- melt(Thermsumm, id.vars=c("Indiv_ID", "Date", "Time"), 
                     measure.vars = c("Min", "Mean","Max"))
-
-m.thermsumm$Hour <- substr(m.thermsumm$Time,1,2)
-m.thermsumm$Hour <- factor(m.thermsumm$Hour, 
+  m.thermsumm$Hour <- substr(m.thermsumm$Time,1,2)
+  m.thermsumm$Hour <- factor(m.thermsumm$Hour, 
                             levels= c("19", "20", "21", "22", "23", "24", "01", "02",
                                       "03", "04", "05", "06"), ordered=T)
+  file.name <- paste(m.thermsumm$Indiv_ID[1], "_", m.thermsumm$Date[1], "_summ.rds", sep="")
+  saveRDS(m.thermsumm,file.name)
+}
 
-ggplot(m.thermsumm, aes(as.character(Time), value)) +
+out<- readRDS(file=paste(bird_id, "_summ.rds", sep=""))
+
+out$Hour <- factor(out$Hour, 
+                           levels= c("19", "20", "21", "22", "23", "24", "01", "02",
+                                     "03", "04", "05", "06"), ordered=T)
+#test_time <- as.POSIXct(out$Time,format='%H%M')
+
+ggplot(out, aes(Time, value)) +
   geom_point(aes(col=variable), size=3) + my_theme +
-  theme(axis.text.x = element_text(angle=30, size=15)) +
-  ylab(Temp.lab) + xlab("Hour")
+  theme(axis.text.x = element_text(angle=60, size=15, vjust=0.5)) +
+  theme(panel.grid.major.y = element_line(colour="grey", size=0.5),
+        axis.text.y=element_text(size=15)) +
+  scale_y_continuous(breaks = c(5,10,15,20,21,22,23,24,25,26,27,28,29,30,35)) +
+  ylab(Temp.lab) + xlab("Hour") + ggtitle(out$Indiv_ID[1])
