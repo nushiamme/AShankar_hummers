ac25943932eb49bcbec639b1b8b60e1343d66e8a
Anusha Shankar <nushiamme@gmail.com>
2018-10-22 02:54:10 -0400

    Interpolated points so that I have surf and amb values per min for all nights. And caculated prop time spent in each category, and ran ancova

--- ThermalFiles_summarize.R
+++ ThermalFiles_summarize.R
@@ -7,7 +7,7 @@ library(dplyr)
 library(reshape2)
 library(ggplot2)
 library(tidyverse)
-library(viridis)
+library(viridis) # Colors
 
 wd <- file.path("E:", "Google Drive", "IR_2018_csv")
 setwd(wd)
@@ -147,6 +147,7 @@ surface_amb <- subset(surface_amb, select = -Row.names)
 head(surface_amb)
 #### Don't use before this ####
 
+
 ## Stacking all the individual birds' data, keeping all melted columns from earlier (hour, min, max, etc.)
 out_all <- data.frame(matrix(ncol = 6, nrow=109*length(bird.folders.all)))
 names(out_all) <- names(out)
@@ -180,8 +181,107 @@ out_full$Indiv_ID <- lapply(out_full$Indiv_ID, function(x) {
 })
 out_full$pasted <- paste(out_full$Indiv_ID, "_", out_full$Date, out_full$Year, sep="")
 head(out_full)
-out_full <- out_full[out_full$pasted != "BCHU05_060718",]
-out_full$Indiv_numeric <- cumsum(!duplicated(out_full$pasted)) ## Making individual column numeric for the anova
+
+#### GOOD interpolate function ####
+
+datatry <- data.frame()
+
+for(j in 1:length(unique(out_full$pasted))) {
+  for(i in unique(out_full$pasted)) {  
+  ## Create trial column
+    trial <- out_full[out_full$pasted==i,]
+    trial$Time <- as.numeric(as.character(trial$Time))
+    times <- c(seq(1930,2400,1), seq(100,530,1))
+    sur_temps <- trial$Surf_Temp
+    amb_temps <- trial$Amb_Temp
+    #temps <- as.data.frame(trial$value)
+    names(sur_temps) <- "Surf_Temp"
+    names(amb_temps) <- "Amb_Temp"
+    time2 <- trial$Time
+    
+    ## Interpolate missing surface temperatures
+    sfun <- approxfun(time2, sur_temps, rule = 2)
+    
+    ## Interpolate missing ambient temperatures
+    afun <- approxfun(time2, amb_temps, rule = 2)
+    
+    ## Ordering time in the interpolated data frame
+    TimeOrder1 <- seq(from = 1900, to = 2459, by = 1) ## Create seq for first half of night
+    TimeOrder2 <- seq(from = 100, to = 559, by = 1) ## Seq for second half of night
+    TimeOrder <- c(TimeOrder1, paste0("0", TimeOrder2)) ## Combine them, making all same length
+    TimeOrder <- factor(TimeOrder, as.character(TimeOrder)) ## Make into a factor in the correct order
+    
+    Time_unordered<- as.factor(format(seq.POSIXt(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date()+1), by = "1 min"),"%H%M", tz="GMT")) ## Make minute-by-minute to compare
+    
+    #TimeFinal <- droplevels(na.omit(TimeOrder[match(Time_unordered, TimeOrder,nomatch=NA)])) ## Final time variable
+    birdTime <- as.character(times) ## Save the interpolated time as a separate vector
+    birdTime <- str_pad(birdTime, width=4, side="left", pad="0") ## Make sure all times are 4 characters long, otherwise pad in front with zero
+    ## Compile interpolated df
+    interTemp <- data.frame(Indiv_pasted = i,
+                            Surf_Temp = sfun(times),
+                            Amb_Temp = afun(times))
+    interTemp$Category <- 0
+    
+    for(k in 1:nrow(interTemp)) {
+        categ <- categories[categories$Individual==i,]
+        if(interTemp$Surf_Temp[k] > categ$Normo_min) {
+          interTemp$Category[k] <- "Normothermic"
+        } else if(!is.na(categ$Shallow_min) & interTemp$Surf_Temp[k] > categ$Shallow_min) {
+          interTemp$Category[k] <- "Shallow"
+        } else if(is.na(categ$Shallow_min) & !is.na(categ$Shallow_max) & interTemp$Surf_Temp[k] < categ$Shallow_max) {
+          interTemp$Category[k] <- "Shallow"
+        } else if(!is.na(categ$Transition_min) & interTemp$Surf_Temp[k] > categ$Transition_min) {
+          interTemp$Category[k] <- "Transition"
+        } else if(is.na(categ$Transition_min) & !is.na(categ$Transition_max) & interTemp$Surf_Temp[k] < categ$Transition_max) {
+          interTemp$Category[k] <- "Transition"
+        } else if(!is.na(categ$Torpor_max) & interTemp$Surf_Temp[k] < categ$Torpor_max) {
+          interTemp$Category[k] <- "Torpor"
+        }
+      }
+    interTemp$Time <- TimeOrder[match(birdTime,TimeOrder,nomatch=NA)] ## Match times to correct ordered time and save into new column in interpolated dataset
+    interTemp$Species <- substr(interTemp$Indiv_pasted, 1, 4)
+    datatry <- rbind(datatry, interTemp) # add it to your df
+  }
+}
+
+## Check if there's any NAs. If sfun and afun have rule=1, yields NAs
+sum(is.na(datatry$Amb_Temp))
+sum(is.na(datatry$Surf_Temp))
+
+#datatry <- dplyr::bind_rows(datalist) ## Would be faster if I figure out how to do make a list in the loop and compile here.
+
+## Summarize proportion of time spent in each state, by species
+data_species_summ <- datatry %>% 
+  group_by(Category,Species) %>%
+  summarise(prop_values = (length(Category)/length(datatry$Surf_Temp))*100)
+
+## By individual
+data_indiv_summ <- datatry %>%
+  group_by(Species, Category) %>%
+  summarise (n = length(Category))
+data_indiv_summ
+
+datatry$Category <- factor(datatry$Category, levels = c("Normothermic", "Shallow", "Transition", "Torpor"))
+my_colors2 <- c("#23988aff", "#F38BA8", "#440558ff", "#9ed93aff")
+ggplot(datatry, aes(Indiv_pasted, Surf_Temp)) + my_theme + geom_point(aes(col=Category), alpha=0.8) +  
+  facet_grid(.~Species, scales = "free_x",space = "free_x") +
+  ylab(Temp.lab) + xlab("Individual") + 
+  #scale_color_manual(values = c('black','deepskyblue2', 'palegreen4', 'red')) +
+  scale_color_manual(values=my_colors2) +
+  guides(colour = guide_legend(override.aes = list(size=3))) +
+  theme(axis.text.x = element_text(angle=90, size=15, vjust=0.5), axis.text.y=element_text(size=15),
+        legend.key.height = unit(3, 'lines'))
+
+
+## IGNORE
+## To compare interpolated data with test data:
+#trial$Time3 <- TimeOrder[match(str_pad(as.character(trial$Time, width=4, side="left", pad="0"),TimeOrder,nomatch=NA)] 
+## Plot interpolated data
+ggplot(interTemp, aes(Time2, Temp)) + 
+  geom_point(alpha = 0.2, col = "red") +
+  ggtitle("Temperature time") +
+  my_theme + 
+  theme(axis.text.x = element_text(angle=90, size=15, vjust=0.5))
 
 
 ## Loops to fill in a "Category" column in the out_full dataset so that each surface temperature is 
@@ -206,18 +306,47 @@ for(i in 1:nrow(out_full)) {
     }
 }
 
-summary(lm(Surf_Temp~Amb_Temp + Category + (1|Indiv_numeric), data=out_full))
+## Running an ancova on Surface ~ Ambient temperature
+out_full$Indiv_numeric <- cumsum(!duplicated(out_full$pasted)) ## Making individual column numeric for the ancova, but this turns out to be unnecessary
+out_full$Species <- substr(out_full$Indiv_ID, 1, 4) ## Making a species column
+out_full$Species_numeric <- cumsum(!duplicated(out_full$Species))
+out_full$Category_Traditional <- out_full$Category
+out_full$Category_Traditional[out_full$Category=="Shallow"] <- "Normothermic"
+out_full$Category_Traditional2 <- out_full$Category
+out_full$Category_Traditional2[out_full$Category=="Shallow"] <- "Torpor"
+
+## including + (1|Indiv_numeric) or + (1|Species_numeric) yields singularities, meaning they are unnecessary variables
+mod.surf_amb <- lm(Surf_Temp~Amb_Temp + Category, data=out_full)
+summary(mod.surf_amb)
+plot(mod.surf_amb)
+
+## Including traditional categories (i.e. considering Shallow is Normo)
+mod.surf_amb_trad <- lm(Surf_Temp~Amb_Temp + Category_Traditional, data=out_full)
+summary(mod.surf_amb_trad)
+plot(mod.surf_amb_trad)
+
+## Including traditional categories (i.e. considering Shallow is torpid)
+mod.surf_amb_trad2 <- lm(Surf_Temp~Amb_Temp + Category_Traditional2, data=out_full)
+summary(mod.surf_amb_trad2)
+plot(mod.surf_amb_trad2)
+
+
+# Other useful functions 
+coefficients(mod.surf_amb) # model coefficients
+confint(mod.surf_amb, level=0.95) # CIs for model parameters 
+anova(mod.surf_amb) # anova table 
+vcov(mod.surf_amb) # covariance matrix for model parameters 
 
 out_full$Category <- factor(out_full$Category, levels = c("Normothermic", "Shallow", "Transition", "Torpor"))
-my_colors <- c("#85d349ff", "#440154ff", "#fde725ff", "#23988aff")
+#my_colors <- c("#85d349ff", "#440154ff", "#fde725ff", "#23988aff")
 my_colors2 <- c("#23988aff", "#F38BA8", "#440558ff", "#9ed93aff")
 
 
 ## Plot surface vs ambient temperature
-ggplot(out_full, aes(Amb_Temp, Surf_Temp)) + geom_point(aes(col=Category, shape=Category), size=2.5) + my_theme +
+ggplot(out_full, aes(Amb_Temp, Surf_Temp)) + geom_point(aes(col=Category_Traditional, shape=Category_Traditional), size=2.5) + my_theme +
   scale_y_continuous(breaks = c(5,10,15,20,21,22,23,24,25,26,27,28,29,30,35,40)) +
   scale_colour_manual(values=my_colors2) +
-  geom_smooth(aes(group=Category),method='lm') +
+  geom_smooth(aes(group=Category_Traditional),method='lm') +
   scale_shape_manual(values = c(15:18)) +
   theme(panel.grid.major.y = element_line(colour="grey", size=0.5), axis.text.x=element_text(size=15),
         axis.text.y=element_text(size=15), legend.key.height = unit(1.5, 'lines')) +
