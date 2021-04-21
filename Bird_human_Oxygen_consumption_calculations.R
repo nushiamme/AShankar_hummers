cdb3b7f9f81ad45e802ce0939a28dc1eab08ff31
Anusha Shankar <nushiamme@gmail.com>
2021-04-21 12:56:34 -0400

    Added calculations on bird O2 consumption compared to humans

---
+++ Bird_human_Oxygen_consumption_calculations.R
@@ -0,0 +1,69 @@
+# As you said initially, atmospheric air consistently has 21% O2 in today's world :)
+# 
+# Human tidal volume (i.e. per breath) is 0.5L. So inspired air for a single human breath has
+# 0.21*500
+# 105 mL of O2 per 500 mL of air.
+# 
+# But in a minute, humans breathe in about 6-12L of air at rest.
+# There's 
+# 0.21*6000 or 1260 mL of O2 in 6000 mL or 6L of (inhaled) air.
+# 
+# Resting humans have a a VO2 of about 3.5 mL/kg/min
+# So *per kg*, humans consume about 3.5 mL of O2 per min. If we assume a 60kg human, their resting VO2 consumption is
+# 3.5*60, or 210 mL O2/min, which means that when they breathed in 6L of air, the air they breathed out had
+# (1260-210)/6000 or 17.5% O2.
+# 
+# Humans VO2 max (i.e. maximum possible oxygen consumption during activity) 
+# is an average of 35-40 mL O2/kg/min for high-performing atheletes (though the highest ever measured was 92mL/kg/min)
+# So per kg, the human consumes 35 mL O2 per min. 
+# For our 60kg human, that's 
+# 35*60 or 2400 mL/min of O2 consumed during heavy exercise.
+# 
+# And in the exhaled air, that's
+# (1260-2400)/6000
+# 
+# -19% O2. Which... doesn't make any sense. But that's why your breathing rate has to increase!! 
+# And it increases to about 100 breaths/ min when you exercise. So you're actually breathing in
+# 100*0.5 or 50L of air instead of the original 6-12L of air.
+# 
+# With that new number, the inhaled air has
+# 0.21*50000
+# 
+# 10,500mL of O2 in 50,000 mL of inhaled air.
+# And in the exhaled air there's
+# 
+# (10500-2400)/50000
+# 
+# 16.2% O2!
+# 
+# I'm guessing that the 16.5% you found was an average over the course of a normal human day. But as you can see, it's more complex than that!
+# 
+# Now onto hummingbirds:
+# From these two references, I'll pulling some average hummingbird tidal volume and breathing rate measurements
+# https://www.researchgate.net/publication/251143060_Respiration_frequency_and_tidal_volume_in_resting_hummingbirds_during_daytime
+# And Schuchmann & Prinzinger. Energy metabolism, nocturnal torpor and respiration frequency in a Green Hermit (Phaethornis guy). J Ornithol. 1988;129: 469–472. 
+# A hummingbird's tidal volume is 0.17 mL. Let's say they're breathing at 200 breaths per minute.
+# So their per minute inhalation volume is 0.17*200 or 34 mL.
+# 
+# Out of that 34 mL of inhaled air, 
+# 0.21*34 or 7.14 mL is O2
+# 
+# If a 3g hummingbird is resting, its VO2 is about 0.5 ml O2/min.
+# So it is consuming 0.5 mL O2 per min, exhaling air with
+# 
+# (7.14-0.5)/34 or 19.5% O2
+# 
+# 
+# If it's hovering it's consuming about 2.3 mL O2/min.
+# Assuming a constant (unlikely) breathing rate,
+# It is consuming 2.3 mL O2 per min, exhaling air with
+# (7.14-2.3)/34 or 14.2% O2
+# 
+# I'm not sure what exactly their breathing rates at their maximum are, 
+# but from some maybe not accurate-for-hovering studies (https://www.jstor.org/stable/30152332?seq=7#metadata_info_tab_contents), let's say
+# it's 500 breaths per min.
+# 0.17*500 = 85mL of air inhaled per min.
+# 
+# That 85mL of air has 
+# 0.21*85 or 17.85mL of O2.
+# (17.85-2.3)/85 or 18.3% O2
